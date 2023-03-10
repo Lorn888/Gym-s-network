4d11ea3045772f80da16fe63ef25d476dc7b2fbb
Patryk <patrykokpara@hotmail.com>
2023-03-10 21:58:17 +0000

    Added header and logo

--- gym-spot/src/App.jsx
+++ gym-spot/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {  Login, Signup } from './pages'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { logo } from './assets'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <BrowserRouter>
+    <header className='w-full flex justify-between items-center bg-blue sm:px-8 px-4 border-b border-b-[#e6ebf4]'>
+      <Link to= '/'>
+        <img src={logo} alt ='logo' className='w-28 object-contain'/>
+      </Link>
+    </header>
+
+   </BrowserRouter>
   )
 }
 
