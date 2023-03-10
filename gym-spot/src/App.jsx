39a9ce8cf4bd910f88701258fe320be3459afddf
Patryk <patrykokpara@hotmail.com>
2023-03-10 23:19:51 +0000

    Added Sign up and Log in pages

--- gym-spot/src/App.jsx
+++ gym-spot/src/App.jsx
@@ -8,9 +8,9 @@ import { logo } from './assets';
 function App() {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-[white] sm:px-8 px-4 border-b border-b-[#e6ebf4]'>
+      <header className='w-full flex justify-between items-center bg-[white] sm:px-8 px-4 border margin border-b-[#e6ebf4]' >
         <Link to='/'>
-          <img src={logo} alt='logo' className='w-28 object-contain' />
+          <img src={logo} alt='logo' className='w-28 object-contain ' />
         </Link>
         <div style={{ display: 'flex' }}>
           <Link to='/login' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
@@ -21,7 +21,12 @@ function App() {
           </Link>
         </div>
       </header>
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'></main>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path = '/login' element={<Login/>}/>
+          <Route path= '/signup' element ={<Signup/>}/>
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
--- gym-spot/src/pages/Login.jsx
+++ gym-spot/src/pages/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Card, FormField } from '../components'
+ 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const [Card, setCard] = useState(true)    
+const [Field, setField] = useState(false)
+return (
+  <section className='max-w-7xl mx-auto'>
+    <div>
+      <h1 className='font-extrabold text-[#222328] text-[32px]'> Sign up form</h1>
+    </div>
+  </section>
+)
 }
 
 export default Login
