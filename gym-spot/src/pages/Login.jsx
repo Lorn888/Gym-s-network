134a7842e77e275e801ba25a80b44236b835bafa
Patryk <patrykokpara@hotmail.com>
2023-03-11 12:18:01 +0000

    Css imporovements

--- gym-spot/src/pages/Login.jsx
+++ gym-spot/src/pages/Login.jsx
@@ -5,15 +5,15 @@ const Login = () => {
     <section className='max-w-7xl mx-auto'>
     <div>
       <h1 className='font-extrabold text-[#222328] text-[32px]'> Login form</h1>
-      <p className='mt-2 text-[#666e75] text-[16px] max-w[500px] '>Login browsky</p>
+      <p className='mt-2 text-[#666e75] text-[16px] max-w[500px] '>Login browsky !</p>
     </div>
 
-    <div className='mt-16'>
-    <form>
+    <div className='mt-16 border-[red]'  >
+    <form className='w-64 mx-auto '>
 
-      <input placeholder='Email' type="email" id="email" name="email" required />
+      <input className='mb-5' placeholder='Email' type="email" id="email" name="email" required />
   
-      <input placeholder='Password' type="password" id="password" name="password" required />
+      <input className='mb-5' placeholder='Password' type="password" id="password" name="password" required />
   
       
       <input type="submit" value="Submit" className='font-inter font-medium bg-[#45718C] text-white px-4 py-2 rounded-md ml-4'/>
