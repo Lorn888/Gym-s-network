58905e729c5079f2dffe8f013e28fdf55faac62b
Patryk <patrykokpara@hotmail.com>
2023-03-13 21:15:24 +0000

    Added onSubmit event that send a post request

--- gym-spot/src/pages/Login.jsx
+++ gym-spot/src/pages/Login.jsx
@@ -1,27 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/login', { email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section className='max-w-7xl mx-auto'>
-    <div>
-      <h1 className='font-extrabold text-[#222328] text-[32px]'> Login form</h1>
-      <p className='mt-2 text-[#666e75] text-[16px] max-w[500px] '>Login browsky</p>
-    </div>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'> Login form</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px] '>Login browsky</p>
+      </div>
 
-    <div className='mt-16'>
-    <form>
+      <div className='mt-16'>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder='Email'
+            type='email'
+            id='email'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
 
-      <input placeholder='Email' type="email" id="email" name="email" required />
-  
-      <input placeholder='Password' type="password" id="password" name="password" required />
-  
-      
-      <input type="submit" value="Submit" className='font-inter font-medium bg-[#45718C] text-white px-4 py-2 rounded-md ml-4'/>
-    </form>
+          <input
+            placeholder='Password'
+            type='password'
+            id='password'
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
 
-    </div>
-  </section>
-  )
-}
+          <input
+            type='submit'
+            value='Submit'
+            className='font-inter font-medium bg-[#45718C] text-white px-4 py-2 rounded-md ml-4'
+          />
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
