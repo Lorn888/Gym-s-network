b58bffa5b91ee7776fe9dcf09d9402f97cb6bc5f
Patryk <patrykokpara@hotmail.com>
2023-03-14 11:24:07 +0000

    Created an API with node.js and express

--- gym-spot/server/server.js
+++ gym-spot/server/server.js
@@ -1,16 +1,34 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const dotenv = require('dotenv');
 
-// add these lines of code
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Load environment variables
+dotenv.config();
+
+// Create a new Express app
+const app = express();
+
+// Enable CORS
 app.use(cors());
 
-app.post('/login', (req, res) => {
-  // your login logic here
+// Parse incoming request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error(error);
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Start the server
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
