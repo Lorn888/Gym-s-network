8abf53096bb9485a3f3034c4952d5f4a2dc8f7c4
Patryk <patrykokpara@hotmail.com>
2023-03-15 22:51:18 +0000

    Listening to requests and created .env

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,15 @@
+require('dotenv').config()
+
 const express = require ('express')
 
+//express app
 const app = express()
+
+app.get('/', (req,res) => {
+    res.json({msg: 'Welcome to my gym-social-app'})
+})
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on a port 7000')
+})
