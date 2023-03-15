c2df9fbec58bfde4a4e0347aaaa4e1d0386fe3a4
Patryk <patrykokpara@hotmail.com>
2023-03-15 23:04:35 +0000

    Added basic route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require ('express')
+const gymRatInfoRoutes = require ('./routes/gym-rat-i')
 
 //express app
 const app = express()
@@ -11,9 +12,8 @@ app.use((req,res,next) =>{
     next()
 })
 
-app.get('/', (req,res) => {
-    res.json({msg: 'Welcome to my gym-social-app'})
-})
+//handler
+app.use(gymRatInfoRoutes)
 
 //listen for requests
 app.listen(process.env.PORT, () => {
