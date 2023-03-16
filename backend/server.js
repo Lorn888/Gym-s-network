56d8a7e3203ed00ec4f78407e3d28f69046fd23e
Patryk <patrykokpara@hotmail.com>
2023-03-16 23:18:08 +0000

    Prettier

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,34 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require ('express')
-const mongoose = require ('mongoose')
-const gymRatInfoRoutes = require ('./routes/gym-rat-i')
+const express = require("express");
+const mongoose = require("mongoose");
+const gymRatInfoRoutes = require("./routes/gym-rat-i");
 
 //express app
-const app = express()
+const app = express();
 
 //middleware
 
 //attatches body from the requests to request object
-app.use(express.json())
+app.use(express.json());
 
-app.use((req,res,next) =>{
-    console.log(req.path, req.method)
-    next()
-})
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/gym-rat-i', gymRatInfoRoutes)
+app.use("/api/gym-rat-i", gymRatInfoRoutes);
 
 //connsect to db
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        
-        //listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('listening on a port', process.env.PORT)
-        })
-        
-    })
-    .catch((error)=> {
-        console.log(error)
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on a port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
