4c3904591423c1803d74fae2c82d4f0191bc7e69
Patryk <patrykokpara@hotmail.com>
2023-03-16 22:37:17 +0000

    Created logic for get single  GRI in the controllers

--- backend/controllers/gym-rat-controler.js
+++ backend/controllers/gym-rat-controler.js
@@ -1,30 +1,49 @@
-const Gym_rat_info = require('../models/gym-rat-info-models')
+const Gym_rat_info = require("../models/gym-rat-info-models");
 
 //Get all gym rat info
-const getGRI = async (req,res) => {
-    const gri = await  Gym_rat_info.find({}).sort({createdAt: -1})    //blank curly brackets finds
+const getAllGRI = async (req, res) => {
+  const gri = await Gym_rat_info.find({}).sort({ createdAt: -1 }); //blank curly brackets finds
 
-    res.status(200).json(gri)
-} 
+  res.status(200).json(gri);
+};
 
 //Get a single rat info
+const getGRI = async (req, res) => {
+  const { id } = req.params;
+
+  const gri = await Gym_rat_info.findById(id);
+
+  if (!gri) {
+    return res.status(404).json({ error: "No such info" });
+  }
+
+  res.status(200).json(gri);
+};
 
 //Create gym rat info
 const createGRI = async (req, res) => {
-    const {sex, experiance, age, weight, height } = req.body
+  const { sex, experiance, age, weight, height } = req.body;
 
-    try {
-        const gym_rat_info = await Gym_rat_info.create({sex, experiance, age, weight, height })
-        res.status(200).json(gym_rat_info)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-    res.json({mssg:'Post members info'})
-}
+  try {
+    const gym_rat_info = await Gym_rat_info.create({
+      sex,
+      experiance,
+      age,
+      weight,
+      height,
+    });
+    res.status(200).json(gym_rat_info);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  res.json({ mssg: "Post members info" });
+};
 //Delete member info
 
-//Update member info    
+//Update member info
 
 module.exports = {
-    createGRI
-}
+  createGRI,
+  getAllGRI,
+  getGRI
+};
--- backend/routes/gym-rat-i.js
+++ backend/routes/gym-rat-i.js
@@ -1,30 +1,28 @@
-const express = require ('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
-    createGRI,
-} = require ('../controllers/gym-rat-controler')
-//get all members info
-router.get('/', (req, res) => {
-    res.json({mssg:'Get all members info'})
-})
+  createGRI,
+  getAllGRI,
+  getGRI,
+} = require("../controllers/gym-rat-controler");
 
-//get a single members info 
-router.get('/:id', (req, res) => {
-    res.json({mssg:'Get single members info'})
-})
+//get all members info
+router.get("/", getAllGRI)
+
+//get a single members info
+router.get("/:id", getGRI)
 
 //post member info
-router.post('/', createGRI)
+router.post("/", createGRI);
 
 //delets member info
-router.delete('/:id', (req, res) => {
-    res.json({mssg:'Delets members info'})
-})
-
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "Delets members info" });
+});
 
 //Updates member info
-router.patch('/:id', (req, res) => {
-    res.json({mssg:'Updates members info'})
-})
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "Updates members info" });
+});
 
-module.exports = router
+module.exports = router;
