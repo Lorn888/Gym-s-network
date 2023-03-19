13364323f677dab0912f6ee8347f7598cd526230
Patryk <patrykokpara@hotmail.com>
2023-03-19 22:43:11 +0000

    Working on context to update page adter adding inf

---
+++ gym-spot/src/context/GRIContext.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const GRIContext = createContext() 
+
+export const GRIContextProvider = () => {
+    return (
+    <GRIContext.Provider>
+         
+    </GRIContext.Provider>
+)}
--- gym-spot/src/index.jsx
+++ gym-spot/src/index.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {GRIContextProvider} from './context/GRIContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <GRIContextProvider>
+
     <App />
+  
+  </GRIContextProvider>
   </React.StrictMode>,
 )
