75523d0e737f1080aa719bc9ba57332a89b5b70e
Patryk <patrykokpara@hotmail.com>
2023-03-29 19:54:44 +0100

    Succesfully tested logoin route in postman

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 //controler functions
-const {loginUser, signupUser} = require('..controllers/userController')
+const {loginUser, signupUser} = require('../controllers/userController')
 
 
 const router = express.Router()
