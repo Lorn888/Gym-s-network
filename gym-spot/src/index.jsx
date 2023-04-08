8c69feb92e2ab5c9f8d6b2b6fe423db9fba11914
Patryk <patrykokpara@hotmail.com>
2023-04-08 22:41:38 +0100

    Wrapped the entire application in the AuthContextP rovider

--- gym-spot/src/index.jsx
+++ gym-spot/src/index.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GRIContextProvider } from "./context/GRIContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GRIContextProvider>
-      <App />
-    </GRIContextProvider>
+    <AuthContextProvider>
+      <GRIContextProvider>
+        <App />
+      </GRIContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
