52f7d7b9cd17c19210762c5ece12b1ce6129edc4
Patryk <patrykokpara@hotmail.com>
2023-04-09 18:55:51 +0100

    Finished useSignup hook

--- gym-spot/src/hooks/useSignup.js
+++ gym-spot/src/hooks/useSignup.js
@@ -1,31 +1,36 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
-export const useSignup = () =>{
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-	const {dispatch} = useAuthContext()
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
 
-    const signup = async (email, password) => {
-        setIsLoading(true)
-        setError(null)
+  const signup = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
 
-        const response = await fetch('http://192.168.0.229:7000/api/user/signup', {
-            method: 'POST',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify({email, password})
-        })
+    const response = await fetch("http://192.168.0.229:7000/api/user/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
-        const json = await response.json()
+    const json = await response.json();
 
-        if(!response.ok) {
-            setIsLoading(false)
-            setError(json.error)
-        }
-        if (response.ok) {
-            //save the user to the local storage 
-            localStorage.setItem('user'. JSON.stringify(json))
-        }
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
     }
-    
-}
+    if (response.ok) {
+      //save the user to the local storage
+      localStorage.setItem("user".JSON.stringify(json));
+      //update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      setIsLoading(false);
+    }
+  };
+
+  return { signup, isLoading, error };
+};
