f12dff98424d7abac36e452aeacbffa7106092a2
Patryk <patrykokpara@hotmail.com>
2023-04-09 18:59:18 +0100

    Used useSignup hook inside the Signup component

--- gym-spot/src/pages/Signup.jsx
+++ gym-spot/src/pages/Signup.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password)
-
+		await signup(email, password)
   };
 
   return (
