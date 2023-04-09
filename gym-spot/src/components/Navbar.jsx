930b73d3d98c353cb8a2a1f596d4bd63b06c0cd1
Patryk <patrykokpara@hotmail.com>
2023-04-09 20:11:12 +0100

    Removing user from sotrage

--- gym-spot/src/components/Navbar.jsx
+++ gym-spot/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { logo } from "../assets";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header className="w-full flex justify-between items-center bg-[white] sm:px-8 px-4 border margin border-b-[#e6ebf4]">
       <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain " />
       </Link>
       <div style={{ display: "flex" }}>
+      <div>
+        <button
+          className="font-inter font-medium bg-[#45718C] text-white px-4 py-2 rounded-md"
+          onClick={handleClick}
+        >
+          Log out
+        </button>
+      </div>
         <Link
           to="/login"
           className="font-inter font-medium bg-[#45718C] text-white px-4 py-2 rounded-md"
