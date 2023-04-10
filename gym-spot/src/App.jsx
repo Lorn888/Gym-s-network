05ca968dd8325c8dd596a1053c13e69dde6feb8b
Patryk <patrykokpara@hotmail.com>
2023-04-10 22:21:14 +0100

    Updated global Gri state

--- gym-spot/src/App.jsx
+++ gym-spot/src/App.jsx
@@ -1,26 +1,21 @@
 import "./App.css";
 import { Login, Signup, Home } from "./pages";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
 import { Navbar } from "./components";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <BrowserRouter>
       <Navbar />
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
-          <Route 
-          path="/" 
-          element={<Home />} />
-          
-          <Route 
-          path="/login" 
-          element={<Login />} />
+          <Route path="/" element={user ? <Home />: <Navigate to = '/login'/>} />
 
-          <Route 
-          path="/signup" 
-          element={<Signup />} />
-          
+          <Route path="/login" element={!user ? <Login />: <Navigate to = '/'/>} />
+
+          <Route path="/signup" element={!user ? <Signup /> : <Navigate to ='/'/>} />
         </Routes>
       </main>
     </BrowserRouter>
